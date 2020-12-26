f0baf653d001f902bd64356b2c8b9d1ba805ea17
Lipcoder <sb921204@naver.com>
2020-12-26 23:18:32 +0900

    20201226

---
+++ Lookation/src/com/test/mvc/SampleController.java
@@ -0,0 +1,25 @@
+// SampleController.java
+// - 사용자 정의 컨트롤러 클래스
+
+
+package com.test.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+//※ Spring 이 제공하는 『Controller』 인터페이스를 구현함으로써
+//사용자 정의 컨트롤러 클래스를 구성한다.
+
+public class SampleController implements Controller
+{
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+}
