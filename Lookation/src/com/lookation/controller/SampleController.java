0adc44cb10ce957ee63c393d8f0aa1f5eec4b0cc
Lipcoder <sb921204@naver.com>
2021-01-11 16:14:48 +0900

    20210111
    1. spring 프레임워크 기본 세팅

--- Lookation/src/com/lookation/controller/SampleController.java
+++ Lookation/src/com/lookation/controller/SampleController.java
@@ -2,7 +2,7 @@
 // - 사용자 정의 컨트롤러 클래스
 
 
-package com.test.mvc;
+package com.lookation.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,15 +10,38 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+import com.lookation.dao.ISampleDAO;
+
 //※ Spring 이 제공하는 『Controller』 인터페이스를 구현함으로써
 //사용자 정의 컨트롤러 클래스를 구성한다.
 
 public class SampleController implements Controller
 {
+	private ISampleDAO dao;
+	
+	public void setDao(ISampleDAO dao)
+	{
+		this.dao = dao;
+	}
+
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
 	{
 		ModelAndView mav = new ModelAndView();
+		
+		try
+		{
+			// jdbc 처리
+			// dao.lists();
+			//...
+			
+			// 불러올 view 세팅 
+			// (jsp이름 or redirect:[액션명].action
+			mav.setViewName("user/mypageUser");
+		} catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
 		
 		return mav;
 	}
---
+++ Lookation/src/com/lookation/dto/SampleDTO.java
@@ -0,0 +1,17 @@
+package com.lookation.dto;
+
+public class SampleDTO
+{
+	// 객체로 처리할 데이터 선언
+	private int test;
+
+	public int getTest()
+	{
+		return test;
+	}
+
+	public void setTest(int test)
+	{
+		this.test = test;
+	}
+}
