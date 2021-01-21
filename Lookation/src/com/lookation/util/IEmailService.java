f22f6118b2c679b2068313aecc1760e1f888a2a3
Lipcoder <sb921204@naver.com>
2021-01-21 21:10:39 +0900

    20210121
    1. change emailService to emailManager

--- Lookation/src/com/lookation/util/IEmailService.java
+++
@@ -1,9 +0,0 @@
-package com.lookation.util;
-
-
-public interface IEmailService
-{
-	public void sendSimple(String to, String from, String subject, String content);
-	public void sendAttach(String to, String from, String subject, String content, String filePath);
-	public void sendHtml(String to, String from, String subject, String html);
-}
